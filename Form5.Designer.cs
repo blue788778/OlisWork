e00c64c6ed376650c901206ac3c58df921fd7058
Oli <User@LAPTOP-MU5UT9K0>
2022-03-30 15:03:58 +0800

    1

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -39,18 +39,20 @@
             // 
             // videoSourcePlayer1
             // 
-            this.videoSourcePlayer1.Location = new System.Drawing.Point(288, 98);
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(56, 44);
+            this.videoSourcePlayer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.videoSourcePlayer1.Name = "videoSourcePlayer1";
-            this.videoSourcePlayer1.Size = new System.Drawing.Size(75, 23);
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(719, 257);
             this.videoSourcePlayer1.TabIndex = 0;
             this.videoSourcePlayer1.Text = "videoSourcePlayer1";
             this.videoSourcePlayer1.VideoSource = null;
             // 
             // button_Open
             // 
-            this.button_Open.Location = new System.Drawing.Point(101, 276);
+            this.button_Open.Location = new System.Drawing.Point(135, 345);
+            this.button_Open.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_Open.Name = "button_Open";
-            this.button_Open.Size = new System.Drawing.Size(75, 23);
+            this.button_Open.Size = new System.Drawing.Size(100, 29);
             this.button_Open.TabIndex = 1;
             this.button_Open.Text = "Open";
             this.button_Open.UseVisualStyleBackColor = true;
@@ -59,53 +61,59 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(101, 327);
+            this.comboBox1.Location = new System.Drawing.Point(135, 409);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.Size = new System.Drawing.Size(160, 23);
             this.comboBox1.TabIndex = 2;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button_Pic
             // 
-            this.button_Pic.Location = new System.Drawing.Point(101, 362);
+            this.button_Pic.Location = new System.Drawing.Point(135, 452);
+            this.button_Pic.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_Pic.Name = "button_Pic";
-            this.button_Pic.Size = new System.Drawing.Size(75, 23);
+            this.button_Pic.Size = new System.Drawing.Size(100, 29);
             this.button_Pic.TabIndex = 3;
             this.button_Pic.Text = "Pic";
             this.button_Pic.UseVisualStyleBackColor = true;
+            this.button_Pic.Click += new System.EventHandler(this.button_Pic_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 287);
+            this.label1.Location = new System.Drawing.Point(37, 359);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.Size = new System.Drawing.Size(67, 15);
             this.label1.TabIndex = 4;
             this.label1.Text = "開啟鏡頭";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 327);
+            this.label2.Location = new System.Drawing.Point(37, 409);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.Size = new System.Drawing.Size(52, 15);
             this.label2.TabIndex = 5;
             this.label2.Text = "解析度";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(28, 362);
+            this.label3.Location = new System.Drawing.Point(37, 452);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.Size = new System.Drawing.Size(37, 15);
             this.label3.TabIndex = 6;
             this.label3.Text = "拍照";
             // 
             // Form5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 562);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -113,6 +121,7 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button_Open);
             this.Controls.Add(this.videoSourcePlayer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form5";
             this.Text = "Form5";
             this.Load += new System.EventHandler(this.Form5_Load);
--- Form5.cs
+++ Form5.cs
@@ -32,16 +32,46 @@ namespace OlisWork
             videoSource = new VideoCaptureDevice(videoDevices[selectedDeviceIndex].MonikerString);      // 連接攝像頭。
             videoSource.VideoResolution = videoSource.VideoCapabilities[selectedDeviceIndex];
             videoSourcePlayer1.VideoSource = videoSource;
+            for (int i = 0; i < videoSource.VideoCapabilities.Length; i++)
+            {
+                //string resolution = "Resolution Number" + Convert.ToString(i);
+                string resolution_size = videoSource.VideoCapabilities[i].FrameSize.ToString();
+                comboBox1.Items.Add(resolution_size);
+            }
         }
-
+        /// <summary>
+        /// 123
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            videoSource.VideoResolution = videoSource.VideoCapabilities[comboBox1.SelectedIndex];
+            videoSourcePlayer1.VideoSource = videoSource;
+            videoSourcePlayer1.Start();
         }
 
         private void button_Open_Click(object sender, EventArgs e)
         {
             videoSourcePlayer1.Start();
+        }
+
+        private void button_Pic_Click(object sender, EventArgs e)
+        {
+            if (videoSource == null)
+            {
+                return;
+            }
+            Bitmap bitmap = videoSourcePlayer1.GetCurrentVideoFrame();
+            string fileName = "Work" + DateTime.Now.ToString("yyyy-MM-dd") + ".jpg";    // 自訂檔案預設名字
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();              // 給使用者選取儲存檔案的位置 / SaveFileDialog()-初始化這個類別的新執行個體
+            saveFileDialog.FileName = fileName;                                // 存檔時檔案名字=檔案預設名字
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)                // ShowDialog()-呼叫對話方塊 / DialogResult.OK-對話方塊的傳回值
+            {
+                bitmap.Save(saveFileDialog.FileName);                          // 儲存檔案
+                bitmap.Dispose();
+            }
         }
     }
 }
